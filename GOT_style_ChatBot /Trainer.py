93a04619f085c826f0508bc02421bbe7edc3910f
Nikhil Shah <heynikhilshah@gmail.com>
2020-12-10 03:16:19 -0500

    Modular Format

---
+++ GOT_style_ChatBot /Trainer.py
@@ -0,0 +1,65 @@
+import Transformer
+import tensorflow as tf
+
+tf.keras.backend.clear_session()
+NUM_LAYERS = 2
+D_MODEL = 256
+NUM_HEADS = 8
+UNITS = 512
+DROPOUT = 0.1
+
+model = Transformer.transformer(
+    vocab_size=vocab_size,
+    num_layers=NUM_LAYERS,
+    units=UNITS,
+    d_model=D_MODEL,
+    num_heads=NUM_HEADS,
+    dropout=DROPOUT)
+
+
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+
+  def __init__(self, d_model, warmup_steps=4000):
+    super(CustomSchedule, self).__init__()
+
+    self.d_model = d_model
+    self.d_model = tf.cast(self.d_model, tf.float32)
+
+    self.warmup_steps = warmup_steps
+
+  def __call__(self, step):
+    arg1 = tf.math.rsqrt(step)
+    arg2 = step * (self.warmup_steps**-1.5)
+
+    return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+  def get_config(self):
+        return {"d_model": self.d_model}
+
+
+def loss_function(y_true, y_pred):
+    y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
+
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none')(y_true, y_pred)
+
+    mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
+    loss = tf.multiply(loss, mask)
+
+    return tf.reduce_mean(loss)
+
+
+optimizer = tf.keras.optimizers.Adam(
+    0.001, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+
+def accuracy(y_true, y_pred):
+  # ensure labels have shape (batch_size, MAX_LENGTH - 1)
+  y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
+  return tf.keras.metrics.sparse_categorical_accuracy(y_true, y_pred)
+
+model.compile(optimizer=optimizer, loss=loss_function, metrics=[accuracy])
+
+EPOCHS = 20
+
+model.fit(dataset, epochs=EPOCHS)
+model.save('transformer')
---
+++ GOT_style_ChatBot /evaluate.py
@@ -0,0 +1,71 @@
+import Preprocessing
+import tensorflow as tf
+
+path = 'full_script.json'
+
+MAX_LENGTH = 50
+
+preprocessor = Preprocessing.Preprocessor(path)
+
+questions, answers = preprocessor.load_dialouges()
+
+print(questions[20])
+print(answers[20])
+
+tokenizer, VOCAB_SIZE, START_TOKEN, END_TOKEN = preprocessor.tokenizer_builder()
+
+
+def loss_function(y_true, y_pred):
+    y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
+
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none')(y_true, y_pred)
+
+    mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
+    loss = tf.multiply(loss, mask)
+
+    return tf.reduce_mean(loss)
+
+
+model = tf.keras.models.load_model('model', custom_objects={'loss_function': loss_function})
+
+
+def evaluate(sentence):
+    sentence = preprocessor.preprocess_sentence(sentence)
+
+    sentence = tf.expand_dims(
+        START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
+
+    output = tf.expand_dims(START_TOKEN, 0)
+
+    for i in range(MAX_LENGTH):
+        predictions = model(inputs=[sentence, output], training=False)
+
+        # select the last word from the seq_len dimension
+        predictions = predictions[:, -1:, :]
+        predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+        # return the result if the predicted_id is equal to the end token
+        if tf.equal(predicted_id, END_TOKEN[0]):
+            break
+
+        # concatenated the predicted_id to the output which is given to the decoder
+        # as its input.
+        output = tf.concat([output, predicted_id], axis=-1)
+
+    return tf.squeeze(output, axis=0)
+
+
+def predict(sentence):
+    prediction = evaluate(sentence)
+
+    predicted_sentence = tokenizer.decode(
+        [i for i in prediction if i < tokenizer.vocab_size])
+
+    print('Input: {}'.format(sentence))
+    print('Output: {}'.format(predicted_sentence))
+
+    return predicted_sentence
+
+
+output = predict("Any man who must say, I am the king, is no true king")
